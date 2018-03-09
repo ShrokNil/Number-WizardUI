20416c31faffecd66371e9207968976f1a7307ba
Shad <31967507+ShrokNil@users.noreply.github.com>
2018-03-09 01:14:56 -0700

    Add files via upload

---
+++ LevelManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+public void Loadlevel (string name) {
+		Debug.Log("Level Code requested for "+name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("I want to quit!");
+		Application.Quit();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+	}
+}
---
+++ NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+	
+	public int maxGuessesAllowed = 25;
+	
+	public Text text;
+	
+	void Start() {
+		StartGame();
+		NextGuess();
+	}
+	
+	void StartGame(){
+		max = 10000;
+		min = 1;
+		guess = 5000;
+		
+	}
+	
+
+
+	public void GuessHigher(){
+			min = guess +1;
+			NextGuess ();
+	}
+	
+	public void GuessLower(){
+		max = guess + 1;
+		NextGuess();
+	}
+		
+	void NextGuess() {
+		guess = Random.Range (min,max);
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if(maxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+}
+	 
+ 
